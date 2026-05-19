fe8f3c6e611b3bd8209f89279830bd8f83ae9518
marcospmelloo <marcospoliveiramello@gmail.com>
2026-05-18 22:21:49 -0300

    chore: configura estrutura inicial do projeto

---
+++ src/main/java/org/serratec/ong_adocao/OngAdocaoApplication.java
@@ -0,0 +1,13 @@
+package org.serratec.ong_adocao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OngAdocaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OngAdocaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/serratec/ong_adocao/OngAdocaoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.ong_adocao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OngAdocaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
