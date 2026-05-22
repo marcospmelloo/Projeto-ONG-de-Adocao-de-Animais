632e9af8f2c3a7103b810f7de47039cff416e754
marcospmelloo <marcospoliveiramello@gmail.com>
2026-05-22 01:16:00 -0300

    feat: cria classe OpenAPIConfig

---
+++ src/main/java/org/serratec/ong_adocao/config/OpenAPIConfig.java
@@ -0,0 +1,34 @@
+package org.serratec.ong_adocao.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI myOpenAPI() {
+        Contact contato = new Contact();
+        contato.setEmail("marcospaulo@email.com.br");
+        contato.setName("Marcos Paulo");
+        contato.setUrl("https://serratec.org.br");
+
+        License apacheLicense = new License()
+                .name("Apache License")
+                .url("https://www.apache.org/license/LICENSE-2.0");
+
+        Info info = new Info()
+                .title("API de ONG de Adoção de Animais")
+                .version("1.0")
+                .contact(contato)
+                .description("API para uma ONG de adoção de animais.")
+                .termsOfService("https://serratec.org.br")
+                .license(apacheLicense);
+
+        return new OpenAPI().info(info);
+    }
+}
